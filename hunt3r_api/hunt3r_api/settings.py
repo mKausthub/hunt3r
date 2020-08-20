7c14ab57372d0b1601473ad03703d523ffa62f46
Kausthub Jadhav <kjadhav@uci.edu>
2020-08-20 02:06:08 -0500

    Update settings.py

--- hunt3r_api/hunt3r_api/settings.py
+++ hunt3r_api/hunt3r_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wrd%6+&0$+5zb*+9^90+3hi9f-qgkx+i5ttgj_2gy3#_0-np8-'
+SECRET_KEY = ######################################################
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
